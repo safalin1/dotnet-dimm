350ab3f4103052bf033fa9ffef06a0bba1cfe89f
Safalin <me@safalin.net>
2025-07-01 23:11:48 +0100

    Initial project

---
+++ src/Netboot/Program.cs
@@ -0,0 +1,8 @@
+﻿using CommandLine;
+
+return Parser
+    .Default
+    .ParseArguments<SendGameDataOptions>(args)
+    .MapResult(
+        _ => 255,
+        _ => 1);
---
+++ src/Netboot/SendGameDataOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using CommandLine;
+
+[Verb("send", HelpText = "Send a game to the netdimm")]
+public class SendGameDataOptions
+{
+    [Option('f', "file", Required = true, HelpText = "Game file to send")]
+    public string? GameFile { get; set; }
+    
+    [Option('d', "ip", Required = true, HelpText = "IP Address of the netdimm you want to send the game to")]
+    public IPAddress? IPAddress { get; set; }
+}
